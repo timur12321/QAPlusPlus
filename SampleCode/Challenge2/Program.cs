7ce8c5249ef2d30cd6e96eb7406a4609f70dc0c5
Neil Syrett <neil.syrett@asos.com>
2016-05-06 15:29:39 +0100

    Sample code moved to SampleCode folder

---
+++ SampleCode/Challenge2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write to console
+            Console.WriteLine("QA++ Challenge 2");
+            Console.WriteLine("================");
+            Console.WriteLine("\nThis program will add, subtract, divide or multiply any two numbers (using an if statement).");
+
+            //Read from console
+            Console.Write("\nEnter a number: ");
+            double number1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter an operator (+ - / *): ");
+            string operation = Console.ReadLine();
+            Console.Write("Enter a second number: ");
+            double number2 = Convert.ToInt32(Console.ReadLine());
+
+            //Calculate result
+            double result;
+            if (operation == "+")
+            {
+                result = number1 + number2;
+            }
+            else if (operation == "-")
+            {
+                result = number1 - number2;
+            }
+            else if (operation == "/")
+            {
+                result = number1/number2;
+            }
+            else if (operation == "*")
+            {
+                result = number1*number2;
+            }
+            else
+            {
+                result = 0;
+            }
+
+            //Print result to command line
+            Console.WriteLine("RESULT = " + result);
+
+            //Exit after Enter
+            Console.WriteLine("\nPress Enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SampleCode/Challenge9/Terrain.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Challenge9
+{
+    public static class Terrain
+    {
+        public static int Height { get; } = 10;
+        public static int Width { get; } = 30;
+
+        public static bool ValidateCoordinates(int x, int y)
+        {
+            if (x < 1 || x > Width || y < 1 || y > Height)
+                return false;
+
+            return true;
+        }
+
+        public static void Draw(Rover rover)
+        {
+            DrawPaddedRow(Terrain.Width, "-", 1, "-");
+
+            for (int y = 1; y <= Terrain.Height; y++)
+            {
+                if (rover.Y == y)
+                {
+                    DrawRow(1, "|");
+                    for (int x = 1; x <= Terrain.Width; x++)
+                    {
+                        if (rover.X == x)
+                        {
+                            DrawRow(1, "X");
+                        }
+                        else
+                        {
+                            DrawRow(1, " ");
+                        }
+                    }
+                    DrawRow(1, "|", true);
+                }
+                else
+                {
+                    DrawPaddedRow(Terrain.Width, " ", 1, "|");
+                }
+            }
+            DrawPaddedRow(Terrain.Width, "-", 1, "-");
+        }
+
+        private static void DrawPaddedRow(int width, string unit, int padding, string paddingUnit)
+        {
+            DrawRow(padding, paddingUnit);
+            DrawRow(width, unit);
+            DrawRow(padding, paddingUnit, true);
+        }
+        private static void DrawRow(int width, string unit, bool endOfLine = false)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                Console.Write(unit);
+            }
+
+            if (endOfLine)
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
