9a2b45a55df5a3ed2e112bc491e9337167cffba8
shaziai <shaziai@asos.com>
2016-06-09 17:34:26 +0100

    Copied code from previous challenge

---
+++ QA++/Challenge4_S/Challenge4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String[] countryValues= new string[2];
+            Console.WriteLine("===========================================");
+            Console.WriteLine("Test Scenario Generator for Eligible Widget ");
+            Console.WriteLine("===========================================");
+
+            Console.WriteLine("Select 2 countries for Default Billing Address in 2 Char Format Eg.- US,DE");
+            
+
+            for (int i = 0; i < 2; i++)
+            {
+                
+                countryValues[i] =Console.ReadLine();
+            }
+            
+
+        
+
+        }
+        public void ResultsGenarator(string country[n])
+        {
+            Console.WriteLine("Test Case: Combination of UK and {1}", country[n]);
+        Console.WriteLine("Test Case: Combination of UK and {1}", country[n]);
+            ResultsGenarator(country[n+1]);
+        }
+
+    }
+}
---
+++ QA++/Shazia- Challenge2/Challenge2/Challenge2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gradeSystem
+{
+    class gradeSystem
+    {
+        static void Main(string[] args)
+
+
+        {
+            Console.WriteLine("=======================");
+            Console.WriteLine("Student Greading System");
+            Console.WriteLine("=======================");
+            
+            Console.WriteLine("Please Enter you Age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            
+            if (age == 3)
+            {
+                Console.WriteLine("You belong to Nursery stage");
+            }
+
+            else if (age == 4)
+            {
+                Console.WriteLine("You belong to Reception stage");
+            }
+
+            else if (age >= 5 & age <= 11)
+            {
+                Console.WriteLine("You belong to Primary school");
+            }
+
+            else if (age >= 12 & age <= 18)
+            {
+                Console.WriteLine("You belong to Secondary school");
+            }
+
+            else if (age > 18)
+            {
+                Console.WriteLine("You are an adult! You do not qualify");
+            }
+
+            else
+            {
+                Console.WriteLine("You have entered an invalid age");
+            }
+            Console.WriteLine("Please Enter you Grade");
+            char grade = Char.Parse(Console.ReadLine());
+
+            switch (grade)
+
+            {
+                case 'A':
+                    Console.WriteLine("Well done. You have passed with an 'Outstanding' grade");
+                    break;
+
+
+                case 'B':
+                    Console.WriteLine("Well done. You have passed with a 'Good' grade");
+                    break;
+
+                case 'C':
+                    Console.WriteLine("Well done. You have passed with a 'Satisfactory' grade");
+                    break;
+
+                case 'D':
+                    Console.WriteLine("You failed. Better luck next time!");
+                    break;
+
+                default:
+                    Console.WriteLine("You have entered an invalid grade");
+                    break;
+
+            }
+
+            Console.ReadLine();
+
+        }
+
+      
+    }
+}
